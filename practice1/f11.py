1a57e1f06857499942793b2acbd8812677c24fee
vixelambda <70568758+vixelambda@users.noreply.github.com>
2021-02-15 15:35:38 +0300

    Delete practice1 directory

--- practice1/f11.py
+++
@@ -1,3 +0,0 @@
-import math
-def f11(x):
-    return x**5-x**4/61-((26*x**6+31*x**2)**7+math.cos(x))-(math.log(x**6)+63*x**5)/(x**5-x**3)
--- practice1/f12.py
+++
@@ -1,7 +0,0 @@
-def f12(x):
-    if x<71:
-        return 33*x+57*x**5+20
-    elif 71<=x<148:
-        return 11*x**4+41*x**6+24
-    else:
-        return x**3/53-73*x**7
