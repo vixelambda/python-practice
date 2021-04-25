093088f8896c0d275a259dcf480f63708c70d7f6
vixelambda <70568758+vixelambda@users.noreply.github.com>
2021-04-26 01:50:10 +0300

    Добавление остальных заданий

---
+++ dop_practice3/package/module1.py
@@ -0,0 +1,2 @@
+def func1():
+    print("This is 1st test module")
---
+++ dop_practice3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="package",
+    version="0.0.1",
+    author="Viktor",
+    author_email="vaxyzv@gmail.com",
+    packages=["package"],
+    description="This is a test package for python practice"
+)
